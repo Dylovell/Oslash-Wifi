96064d960a833e5ff6bc56967555695744cc2cfe
Dylan L <Dylovell@live.com>
2018-06-19 00:28:15 -0600

    added picture

--- oslash-wifi/src/components/main.js
+++ oslash-wifi/src/components/main.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import crowd from '../styling/Crowd-rob-curran.jpg'
+
 export default class Main extends Component {
     render() {
         return (
             <div className="Main">
-                <p>MAIN</p>
+                <img alt='Image' src={crowd} width='100%'/>
+                <br/>
+                <p>Want faster WIFI?</p>
+                <p>Make your WIFI faster with this one trick!</p>
+                <p>IPSs hate him!</p>
             </div> 
         )
     }
